b2bf8eec718faa79be66c95f58b130379d1be9d9
choiwono <treasure_b@naver.com>
2018-10-18 10:48:59 +0900

    www/php/branch/freelancer-list.php 수정
    699~709 라인 수정완료
    739~803 라인 수정완료

--- www/php/branch/freelancer-list.php
+++ www/php/branch/freelancer-list.php
@@ -695,15 +695,20 @@ $(document).ready(function() {
                 runMain.fn.freelancerHealthReg(modalTitle,modalObjName,mode,code);
             });
 
-            // 달력
-            $("body").on("click","#birthDate",function(e){
-                // console.log('달력');
-                $(this).removeClass("hasDatepicker").datepicker({
-                    dateFormat: 'yymmdd',
-                    prevText: '<i class="fa fa-chevron-left"></i>',
-					nextText: '<i class="fa fa-chevron-right"></i>'
-                });
-            });
+             // 달력
+			$("body").on("click", "#birthDate, #mnger_ency_dt, #mnger_retire_dt, #mnger_lvl_dt", function(e) {
+				//console.log('달력');
+				$(this).removeClass("hasDatepicker").datepicker({
+					dateFormat: 'yymmdd',
+					prevText: '<i class="fa fa-chevron-left"></i>',
+                    nextText: '<i class="fa fa-chevron-right"></i>',
+                    changeMonth : true,
+                    changeYear : true,
+                    yearRange:"1950:2018",
+                    monthNamesShort: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'],
+                    showButtonPanel: true
+				});
+			});
 
             // 다음 우편번호
             $("body").on("click","#daumPost",function(e){
@@ -731,6 +736,71 @@ $(document).ready(function() {
                         $("#"+modalObjName).html(""); // 타겟코드 초기화
                         $("#"+modalObjName).html(Data); //타겟 소스 input
 						$("#"+modalObjName+"Label").text(modalTitle); // 팝업 타이틀 Set
+
+                        
+                        // Validation
+                        $("#smart-form-register").validate({
+
+                            // Rules for form validation
+                            rules : {
+                                mnger_name : {
+                                    required : true  
+                                },
+                                mnger_birth_dt : {
+                                    required : true
+                                },
+                                mnger_mobile : {
+                                    required : true,
+                                    minlength : 10,
+                                    mobilephone : true
+                                },
+                                mnger_ency_dt : {
+                                    required : true,
+                                    number: true
+                                },
+                                mnger_retire_dt : {
+                                    required : true,
+                                    number: true
+                                }
+                            },
+
+                            // Messages for form validation
+                            messages : {
+                                mnger_name : {
+                                    required : '아이디를 반드시 입력해주세요.'
+                                },
+                                mnger_birth_dt : {
+                                    required : '생년월일을 반드시 입력해주세요.'
+                                },
+                                mnger_mobile : {
+                                    required : '휴대폰번호을 반드시 입력해주세요.'
+                                },
+                                mnger_ency_dt : {
+                                    required : '입사일을 반드시 입력해주세요.'
+                                },
+                                mnger_retire_dt : {
+                                    required : '퇴사일을 반드시 입력해주세요.'
+                                }
+                            },
+
+                            // Ajax form submition
+                            submitHandler : function(form) {
+                                $(form).ajaxSubmit({
+                                    success : function() {
+                                        $("#smart-form-register").addClass('submited');
+                                    }
+                                });
+                            },
+
+                            // Do not change code below
+                            errorPlacement : function(error, element) {
+                                error.insertAfter(element.after());
+                            }
+                        });
+
+                        $('#i-agree').on('click',function(event){
+                            $('#smart-form-register').submit();
+                        });
                     },
                     error: function(XMLHttpRequest, textStatus, errorThrown){
                         alert('Error : ' + errorThrown);
